efa6a274e461bd690c37c77870ef82ece0dcefcd
Emmanuel P. Magadia <emmanuelmagadia@outlook.com>
2019-05-24 16:47:02 +0800

    Test

--- VisualStudio/Program.cs
+++ VisualStudio/Program.cs
@@ -7,6 +7,7 @@ namespace VisualStudio
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
