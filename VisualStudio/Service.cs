0fe3b30258b3191630096736fe758a2d99becc1d
mlomio <mlomio@BAIDEVLP109>
2019-05-27 15:41:29 +0800

    Add service class

---
+++ VisualStudio/Service.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VisualStudio
+{
+    public class Service
+    { 
+        public string GetName(string name)
+        {
+            return name;
+        }
+    }
+}
