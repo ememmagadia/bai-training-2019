d053abbec04e4bfb52a1945730e1517f81df0708
jelmarose <jelmarosedevera@gmail.com>
2019-05-27 16:17:36 +0800

    added services class

---
+++ VisualStudio/Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VisualStudio
+{
+    public class Service
+    {
+        public string GetName(string name)
+        {
+
+            var x = mame + "hello";
+            return x;
+
+        }
+    }
+}
