06c3e593533d5be414178e6f2d9ecab5cad88862
jelmarose <jelmarosedevera@gmail.com>
2019-05-28 11:42:13 +0800

    Exercise 1 - FooBar done

---
+++ FooBar/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace FooBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            int number;
+
+            Console.WriteLine("FOO BAR");
+            Console.WriteLine("If the number is divisible by 3, the output will be FOO.");
+            Console.WriteLine("If the number is divisible by 5, the output will be BAR.");
+            Console.WriteLine("If the number is divisible by 3 AND 5, the output will be FOOBAR.");
+            Console.WriteLine("If the number is neither divisible by 3 OR 5, the output will be the number entered.");
+            Console.WriteLine("To exit the program, type x and press enter.");
+            
+
+            while(input != "x")
+            {
+                Console.Write("Enter number: ");
+                input = Console.ReadLine();
+                if (input == "x")
+                {
+                    Console.WriteLine("Exiting program!");
+                }
+                else
+                {
+                    if(int.TryParse(input, out number))
+                    {
+                        if(number == 0)
+                        {
+                            Console.WriteLine(number);
+                        }
+                        else if(number % 3 == 0 && number % 5 == 0)
+                        {
+                            Console.WriteLine("FOOBAR");
+                        }
+                        else if(number % 3 == 0 && number % 5 != 0)
+                        {
+                            Console.WriteLine("FOO");
+                        }
+                        else if (number % 3 != 0 && number % 5 == 0)
+                        {
+                            Console.WriteLine("BAR");
+                        }
+                        else
+                        {
+                            Console.WriteLine(number);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input! Please enter a number. \n");
+                    }
+                }
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
--- VisualStudio/Program.cs
+++ VisualStudio/Program.cs
@@ -4,9 +4,22 @@ namespace VisualStudio
 {
     class Program
     {
+        public int Sum(int firstNo, int secondNo)
+        {
+            return firstNo + secondNo;
+        }
+        public int Product(int firstNo, int secondNo)
+        {
+            return firstNo * secondNo;
+        }
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello world!");
+            Program p = new Program();
+            int product = p.Product(7, 95);
+            int sum = p.Sum(213, 812);
+            Console.WriteLine("The product of 7 and 95 is " + product);
+            Console.WriteLine("The sum of 213 and 812 is " + sum);
            
         }
     }
