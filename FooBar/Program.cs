0d4106cb2e06ba09bd5ff6efb69d5c776c775c4c
Kyla C <kyla.calpito@gmail.com>
2019-05-28 12:02:48 +0800

    FooBar version 1

---
+++ FooBar/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace FooBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            string loop = "N";
+            int num;
+            bool isNum;
+
+            do
+            {
+                Console.Write("Enter any number: ");
+                isNum = int.TryParse(Console.ReadLine(), out num);
+
+                //Console.WriteLine("Accepted number is " + num);
+
+                if(isNum)
+                {
+                    if (num > 0)
+                    {
+                        string output = GetOutput(num);
+                        Console.WriteLine(output);
+                    }
+                    else
+                    {
+                        Console.WriteLine("I can only process positive numbers.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                }
+                
+
+                //Console.Write("Try again? (Y/N): ");
+                //loop = Console.ReadLine();
+
+            } while (isNum);
+        }
+
+        static string GetOutput(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                return "Output: FooBar";
+            }
+
+            else if (num % 5 == 0)
+            {
+                return "Output: Bar";
+            }
+
+            else if (num % 3 == 0)
+            {
+                return "Output: Foo";
+            }
+
+            else
+            {
+                return "Output: " + num;
+            }
+        }
+    }
+}
