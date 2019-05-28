d163b96457e0d5167830410ece5b67fc636c76f5
amendoza <amendoza@BAIDEVLP107>
2019-05-28 13:01:30 +0800

    Additional library - Exercise 1

---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TestLibrary
+{
+    public class TestClass
+    {
+        public string GetName(string name)
+        {
+            return name;
+        }
+        public string FooBar(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                return "Foobar";
+            }
+            if (num % 3 == 0)
+            {
+                return "Foo";
+            }
+            if (num % 5 == 0)
+            {
+                return "Bar";
+            }
+            else
+            {
+                return num.ToString();
+            }
+        }
+    }
+}
