1c38a784a792ff72dcba338dfacf539a57c2ec26
amendoza <amendoza@BAIDEVLP107>
2019-05-28 13:29:12 +0800

    Exercise 2

---
+++ Exercise1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TestLibrary;
+
+namespace VisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TestClass foo = new TestClass();
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("Input number:");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(foo.FooBar(num));
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine("Input valid number");
+                    }
+                }
+            }
+           
+
+        }
+    }
+}
---
+++ Exercise2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TestLibrary;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TestClass foo = new TestClass();
+            {
+                    try
+                    {
+                        Console.Write("Input number:");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        for(int i = 1; num >= i; i++)
+                        {
+                            Console.WriteLine(foo.FooBar(i));
+                        }
+
+                        Console.WriteLine(foo.FooBar(num));
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine("Input valid number");
+                    }
+                
+            }
+        }
+    }
+}
