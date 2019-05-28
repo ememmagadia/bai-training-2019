7857b7e87a7393ef1c7cbc0ab86e948d072e92a8
amendoza <amendoza@BAIDEVLP107>
2019-05-28 12:59:41 +0800

    Exercise 1 - arnold

--- VisualStudio/Program.cs
+++ VisualStudio/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TestLibrary;
 
 namespace VisualStudio
 {
@@ -6,8 +7,23 @@ namespace VisualStudio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            TestClass foo = new TestClass();
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("Input number:");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(foo.FooBar(num));
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine("Input valid number");
+                    }
+                }
+            }
+           
         }
     }
 }
