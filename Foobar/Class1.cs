0006b5d3005fe78c570f918b2b2b5cb81d1807b6
mlomio <mlomio@BAIDEVLP109>
2019-05-28 12:49:56 +0800

    Done

---
+++ Foobar/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foobar
+{
+    public class Class1
+    {
+        public String Number(String Num1)
+        {
+            bool ValidNumber = false;
+            int Number;
+            if (int.TryParse(Num1, out Number))
+            {
+                ValidNumber = true;
+                Number = Int32.Parse(Num1);
+                if (Number == 0)
+                {
+                    Console.WriteLine("FOOBAR");
+                }
+                else if (Number < 0)
+                {
+                    Console.WriteLine("Please Greater Than zero");
+                }
+                else if (Number % 15 == 0)
+                {
+                    Console.WriteLine("FOOBAR");
+                }
+                else if (Number % 3 == 0)
+                {
+                    Console.WriteLine("FOO");
+
+                }
+                else if (Number % 5 == 0)
+                {
+                    Console.WriteLine("BAR");
+
+                }
+                else
+                {
+                    Console.WriteLine(Num1);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please input  number");
+
+            }
+            return Num1;
+        }
+    }
+}
--- Foobar/Program.cs
+++ Foobar/Program.cs
@@ -4,60 +4,18 @@ namespace Foobar
 {
     class Program
     {
-        public String Number(String Num1)
-        {
-            bool ValidNumber = false;
-            int Number;
-            if (int.TryParse(Num1, out Number))
-            {
-                ValidNumber = true;
-                Number = Int32.Parse(Num1);
-                if (Number == 0)
-                {
-                    Console.WriteLine("FOOBAR");
-                }
-                else if (Number < 0)
-                {
-                    Console.WriteLine("Please Greater Than zero");
-                }
-                else if (Number % 15 == 0)
-                {
-                    Console.WriteLine("FOOBAR");
-                }
-                else if (Number % 3 == 0)
-                {
-                    Console.WriteLine("FOO");
-
-                }
-                else if (Number % 5 == 0)
-                {
-                    Console.WriteLine("BAR");
-                  
-                }
-                else
-                {
-                    Console.WriteLine(Num1);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Please input  number");
-               
-            }
-            return Num1;
-        }
         static void Main(string[] args)
         {
-            Program n = new Program();
+            Class1 n = new Class1();
             int i = 0;
             while (i <= 3)
-             {
-            Console.WriteLine("Enter a number");
+            {
+                Console.WriteLine("Enter a number");
                 string Input = Console.ReadLine();
                 //calling the Number method
                 n.Number(Input);
                 i++;
-               }
+            }
             Console.ReadKey();
 
         }
