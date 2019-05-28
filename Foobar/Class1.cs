79353705a2199e1fd684dbc7d2ab3912ea5ca495
aaroncustodio <custodio.aaron.1197@gmail.com>
2019-05-28 13:47:38 +0800

    Exercise 1 done

---
+++ Foobar/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foobar
+{
+    class Class1
+    {
+        public void DetermineFoobar(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                Console.WriteLine("Foobar");
+            }
+            else if (num % 3 == 0)
+            {
+                Console.WriteLine("Foo");
+            }
+            else if (num % 5 == 0)
+            {
+                Console.WriteLine("bar");
+            }
+            else
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+    class Class2
+    {
+        public void StartMsg()
+        {
+            Console.WriteLine("Type 'exit' to close the program.");
+        }
+        public void InputMsg()
+        {
+            Console.Write("Enter a number: ");
+        }
+        public void ErrorMsg()
+        {
+            Console.WriteLine("Input must be a positive integer.");
+        }
+    }
+}
---
+++ Foobar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Foobar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 Input = new Class1();
+            Class2 Output = new Class2();
+
+            String input = "";
+
+            Output.StartMsg();
+
+            while (input != "exit")
+            {
+                Output.InputMsg();
+
+                input = Console.ReadLine();
+
+                if (Int32.TryParse(input, out int num) && input != "exit" && num >= 0)
+                {
+                    Input.DetermineFoobar(num);
+                }
+                else
+                {
+                    Output.ErrorMsg();
+                }
+            }
+
+        }
+    }
+}
