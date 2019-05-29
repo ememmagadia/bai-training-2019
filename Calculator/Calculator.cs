55e2ac867e069bcd7ac410abf54a64fe9e7b7462
Kyla C <kyla.calpito@gmail.com>
2019-05-29 13:12:50 +0800

    Version 1 of calculator

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    abstract class Calculator
+    {
+        protected int firstNum;
+        protected int secondNum;
+        protected int Result;
+
+        public abstract int Calculate();
+    }
+
+    class Add : Calculator
+    {
+        public Add(int num1, int num2)
+        {
+            firstNum = num1;
+            secondNum = num2;
+        }
+
+        public override int Calculate()
+        {
+            Console.Write("The total for " + firstNum + " and " + secondNum + " is ");
+            return firstNum + secondNum;
+        }
+    }
+
+    class Subtract : Calculator
+    {
+        public Subtract(int num1, int num2)
+        {
+            firstNum = num1;
+            secondNum = num2;
+        }
+
+        public override int Calculate()
+        {
+            Console.Write("The difference between " + firstNum + " and " + secondNum + " is ");
+            return secondNum - firstNum;
+        }
+    }
+
+    class Divide : Calculator
+    {
+        public Divide(int num1, int num2)
+        {
+            firstNum = num1;
+            secondNum = num2;
+        }
+
+        public override int Calculate()
+        {
+            Console.Write("The quotient of " + firstNum + " and " + secondNum + " is ");
+
+            try
+            {
+                return firstNum / secondNum;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.Write("undefined because divisor is ");
+                return 0;
+            }
+            
+        }
+
+    }
+
+    class Multiply : Calculator
+    {
+        public Multiply(int num1, int num2)
+        {
+            firstNum = num1;
+            secondNum = num2;
+        }
+
+        public override int Calculate()
+        {
+            Console.Write("The product of " + firstNum + " and " + secondNum + " is ");
+            return firstNum * secondNum;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool IsValid;
+            int FirstNum;
+            int SecondNum;
+
+            do
+            {
+                // Check if valid number
+                Console.Write("Enter the first number: ");
+                IsValid = int.TryParse(Console.ReadLine(), out FirstNum);
+                CheckNumber(IsValid);
+
+                Console.Write("Enter the second number: ");
+                IsValid = int.TryParse(Console.ReadLine(), out SecondNum);
+                CheckNumber(IsValid);
+
+                // Check if valid operator
+                Console.Write("Enter an operator( + | - | * | / ): ");
+                string CalcOperator = Console.ReadLine().Trim();
+                IsValid = CheckOperator(CalcOperator);
+
+                if (IsValid)
+                {
+                    CallOperation(CalcOperator, FirstNum, SecondNum);
+                }
+
+            } while (IsValid);
+            
+        }
+
+        static void CheckNumber(bool IsValid)
+        {
+            if (!IsValid)
+            {
+                Console.WriteLine("Please enter a valid number");
+                Environment.Exit(0);
+            }
+        }
+
+        static bool CheckOperator(string Optr)
+        {
+            if (Optr == "+" || Optr == "-" || Optr == "*" || Optr == "/")
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid operator.");
+                Environment.Exit(0);
+                return false;
+            }
+        }
+
+        static void CallOperation(string Optr, int FirstNum, int SecondNum)
+        {
+            int Result;
+            if (Optr == "+")
+            {
+                Add add = new Add(FirstNum, SecondNum);
+                Result = add.Calculate();
+                Console.WriteLine(Result);
+            }
+            else if (Optr == "-")
+            {
+                Subtract sub = new Subtract(FirstNum, SecondNum);
+                Result = sub.Calculate();
+                Console.WriteLine(Result);
+            }
+            else if (Optr == "/")
+            {
+                Divide div = new Divide(FirstNum, SecondNum);
+                Result = div.Calculate();
+                Console.WriteLine(Result);
+            }
+            else if (Optr == "*")
+            {
+                Multiply mul = new Multiply(FirstNum, SecondNum);
+                Result = mul.Calculate();
+                Console.WriteLine(Result);
+            }
+        }
+    }
+}
