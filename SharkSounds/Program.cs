e8f6971031008d349f1c5dfc94af3db803a059a3
jelmarose <jelmarosedevera@gmail.com>
2019-05-29 11:16:17 +0800

    Exercise 3 - Shark sounds done

---
+++ SharkSounds/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SharkSounds
+{
+    public interface IMakeSound
+    {
+        void MakeSound();
+    }
+    public class Shark: IMakeSound
+    {
+        public string Name;
+        public string Sound;
+        public void MakeSound()
+        {
+            Console.WriteLine("{0} {1}", Name, Sound);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" - Shark Sounds - ");
+            Shark BabyShark = new Shark
+            {
+                Name = "Baby Shark",
+                Sound = "dooo dooo dooooo"
+            };
+            Shark MommyShark = new Shark
+            {
+                Name = "Mommy Shark",
+                Sound = "doo doo doo doo"
+            };
+            Shark DaddyShark = new Shark
+            {
+                Name = "Daddy Shark",
+                Sound = "doo doo doo doo"
+            };
+
+            BabyShark.MakeSound();
+            MommyShark.MakeSound();
+            DaddyShark.MakeSound();
+        }
+    }
+}
