304b94b5da9ab4cc174fa72d2eed13d651005d60
BlastAsia Cloud02 <baicloud02@outlook.com>
2019-05-29 16:27:00 +0800

    Calculator v1

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,11 +2,83 @@
 
 namespace Calculator
 {
-	class Program
+	abstract class numbers
 	{
-		static void Main(string[] args)
+		public int firstnum;
+		public int secondnum;
+		public abstract int result();
+
+	}
+	class Add : numbers
+	{
+		public override int result()
 		{
-			Console.WriteLine("Hello World!");
+			return (firstnum + secondnum) ;
+		}
+	}
+
+	class Subtract : numbers
+	{
+		public override int result()
+		{
+			return (firstnum - secondnum);
+		}
+	}
+	class Divide : numbers
+	{
+		public override int result()
+		{
+			return (firstnum / secondnum);
+		}
+	}
+	class Multiply : numbers
+	{
+		public override int result()
+		{
+			return (firstnum * secondnum);
+		}
+	}
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			int first, second;
+			string operation;
+			Console.Write("Enter First number: ");
+			first = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Enter Second number: ");
+			second = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Choose an Operation\nA\tfor\tAddition\nB\tfor\tSubtraction\nC\tfor\tDivision\nD\tfor\tMultiplication\nChoice: ");
+			operation = Console.ReadLine();
+			switch(operation)
+			{
+				case "A":	Add add = new Add();
+							add.firstnum = first;
+							add.secondnum = second;
+							Console.WriteLine("Result: " + add.result());
+							break;
+
+				case "B":	Subtract sub = new Subtract();
+							sub.firstnum = first;
+							sub.secondnum = second;
+							Console.WriteLine("Result: " + sub.result());
+							break;
+				case "C":
+					Divide div = new Divide();
+					div.firstnum = first;
+					div.secondnum = second;
+					Console.WriteLine("Result: " + div.result());
+					break;
+				case "D":
+					Multiply multiply = new Multiply();
+					multiply.firstnum = first;
+					multiply.secondnum = second;
+					Console.WriteLine("Result: " + multiply.result());
+					break;
+			}
+
+			Console.ReadKey();
+
 		}
 	}
 }
