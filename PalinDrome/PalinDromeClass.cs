1efc6bb2503dd2c1bc4f9d16a7c529b1b0b3a0f5
mmejidana <mmejidana@BAIDEVLP103>
2019-05-29 15:52:20 +0800

    Activity 2 - PalinDrome

---
+++ PalinDrome/PalinDromeClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalinDrome
+{
+    class PalinDromeClass
+    {
+        public void ReverseWord(int count)
+        {
+            
+        }
+
+        public bool IsMatch(string a, string b)
+        {
+            if (a.Equals(b))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ PalinDrome/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PalinDrome
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            List<string> ListOfPalindrome = new List<string>();
+
+            PalinDromeClass p = new PalinDromeClass();
+
+            ListOfPalindrome.Add("civic");
+            ListOfPalindrome.Add("deified");
+            ListOfPalindrome.Add("deleveled");
+            ListOfPalindrome.Add("devoved");
+            ListOfPalindrome.Add("dewed");
+            ListOfPalindrome.Add("Hannah");
+            ListOfPalindrome.Add("kayak");
+            ListOfPalindrome.Add("level");
+            ListOfPalindrome.Add("madam");
+            ListOfPalindrome.Add("racecar");
+            ListOfPalindrome.Add("radar");
+            ListOfPalindrome.Add("redder");
+            ListOfPalindrome.Add("refer");
+            ListOfPalindrome.Add("repaper");
+            ListOfPalindrome.Add("reviver");
+            ListOfPalindrome.Add("rotator");
+            ListOfPalindrome.Add("rotor");
+            ListOfPalindrome.Add("sagas");
+            ListOfPalindrome.Add("solos");
+            ListOfPalindrome.Add("sexes");
+            ListOfPalindrome.Add("stats");
+            ListOfPalindrome.Add("tenet");
+            ListOfPalindrome.Add("Dot");
+            ListOfPalindrome.Add("A");
+            ListOfPalindrome.Add("Palindrome");
+            ListOfPalindrome.Add("Race caR");
+            ListOfPalindrome.Add("RaCe CaR");
+            ListOfPalindrome.Add("rAce caR");
+
+            for (int c = 0; c < ListOfPalindrome.Count; c++)
+            {
+                string reverse="";
+                string proper = ListOfPalindrome[c];
+                int length = proper.Length;
+
+                while (length > 0)
+                {
+                    reverse += proper[length-1];
+                    length--;
+                }
+
+                proper = Regex.Replace(ListOfPalindrome[c], @"\s", "");
+                reverse = Regex.Replace(reverse, @"\s", "");
+
+                bool result = p.IsMatch(proper, reverse);
+
+                Console.WriteLine("Word: {0} - {1} Result: {2}", proper, reverse, result);
+            }
+
+        }
+
+    }
+}
