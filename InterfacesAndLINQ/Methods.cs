3cd0ba0bdd49d9b038017b757475d5ce656d08ae
aaroncustodio <custodio.aaron.1197@gmail.com>
2019-05-31 10:12:35 +0800

    Get all done

---
+++ InterfacesAndLINQ/Methods.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfacesAndLINQ
+{
+    class InputAndSearch
+    {
+        #region ExecSearch
+        protected static void ExecSearch(IEnumerable<KantoPokes> result)
+        {
+            foreach (var i in result)
+            {
+                Console.WriteLine("Dex no.: {0}\t\tName: {1}\t\t\tType: {2} {3}",
+                    i.DexNo, i.Name, i.Type1, i.Type2);
+            }
+        }
+        protected static void ExecSearch(IEnumerable<JohtoPokes> result)
+        {
+            foreach (var i in result)
+            {
+                Console.WriteLine("Dex no.: {0}\t\tName: {1}\t\t\tType: {2} {3}",
+                    i.DexNo, i.Name, i.Type1, i.Type2);
+            }
+        } 
+        #endregion
+        protected static void GetAll(Program p, Boolean kanto)
+        {
+            if (kanto)
+            {
+                foreach (KantoPokes i in p.box1)
+                {
+                    Console.WriteLine("Dex no.: {0}\t\tName: {1}\t\t\tType: {2} {3}",
+                        i.DexNo, i.Name, i.Type1, i.Type2);
+                }
+            }
+            if (!kanto)
+            {
+                foreach (JohtoPokes i in p.box2)
+                {
+                    Console.WriteLine("Dex no.: {0}\t\tName: {1}\t\t\tType: {2} {3}",
+                        i.DexNo, i.Name, i.Type1, i.Type2);
+                }
+            }
+        }
+        protected static string SearchBy(string region)
+        {
+            string input;
+            while (true)
+            {
+                Console.WriteLine("Search by:");
+                Console.WriteLine("[1] DexNo, [2] Name, [3] Type, [4] List all");
+                Console.Write(">>  ");
+
+                input = Console.ReadLine();
+
+                if (input == "1" || input == "2" || input == "3" || input == "4")
+                {
+                    //Console.WriteLine(region + "" + input);
+                    return region + "" + input;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Try again.\n\n");
+                }
+
+            }
+        }
+        protected static string ChooseRegion()
+        {
+            string input;
+            while (true)
+            {
+                Console.WriteLine("Choose a region:");
+                Console.WriteLine("[1] Kanto, [2] Johto");
+                Console.Write(">>  ");
+
+                input = Console.ReadLine();
+
+                if (input == "1" || input == "2")
+                {
+                    return input;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Try again.\n\n");
+                }
+
+            }
+        }
+        protected static string GetSearchKeywords()
+        {
+            Console.Write("Search: ");
+            return Console.ReadLine();
+        }
+    }
+}
--- InterfacesAndLINQ/Program.cs
+++ InterfacesAndLINQ/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace InterfacesAndLINQ
 {
-    class Program
+    class Program : InputAndSearch
     {
         public IList<KantoPokes> box1 = new List<KantoPokes>()
             {
@@ -35,76 +35,68 @@ namespace InterfacesAndLINQ
                 new JohtoPokes{Name="Noctowl",
                 DexNo="117", Type1 = "Normal", Type2 = "Flying"},
                 new JohtoPokes{Name="Ampharos",
-                DexNo="181", Type1 = "Electric"}
+                DexNo="181", Type1 = "Electric"},
+                new JohtoPokes{Name="Ariados",
+                DexNo="168", Type1 = "Bug", Type2 = "Poison"},
+                new JohtoPokes{Name="Pichu",
+                DexNo="172", Type1 = "Electric"},
+                new JohtoPokes{Name="Xatu",
+                DexNo="178", Type1 = "Psychic", Type2 = "Flying"}
             };
 
         static void Main(string[] args)
         {
             Program p = new Program();
-           
 
             string Region = ChooseRegion();
 
             string Search = SearchBy(Region);
 
-            string Keywords = GetSearchKeywords();
-
-            if (Search == "11")
-            {
-                IEnumerable<KantoPokes> result = p.box1.Where(a => a.DexNo.Contains(Keywords));
-                ExecSearch(p.box1);
-            }
-            if (Search == "12")
-            {
-                IEnumerable<KantoPokes> result = p.box1.Where(a => a.Name.Contains(Keywords));
-                ExecSearch(p.box1);
-            }
-            if (Search == "13")
-            {
-                IEnumerable<KantoPokes> result = p.box1.Where(a => a.Type1.Contains(Keywords));
-                ExecSearch(result);
-            }
             if (Search == "14")
             {
-                
-            }
-            if (Search == "21")
-            {
-                IEnumerable<JohtoPokes> result = p.box2.Where(a => a.DexNo.Contains(Keywords));
-                ExecSearch(p.box2);
-            }
-            if (Search == "22")
-            {
-                IEnumerable<JohtoPokes> result = p.box2.Where(a => a.DexNo.Contains(Keywords));
-                ExecSearch(p.box2);
-            }
-            if (Search == "23")
-            {
-                IEnumerable<JohtoPokes> result = p.box2.Where(a => a.DexNo.Contains(Keywords));
-                ExecSearch(p.box2);
+                GetAll(p, true);
             }
             if (Search == "24")
             {
-
+                GetAll(p, false);
             }
-            //GetAll(p);
+            else
+            {
+                string Keywords = GetSearchKeywords();
 
+                if (Search == "11")
+                {
+                    IEnumerable<KantoPokes> result = p.box1.Where(a => a.DexNo.Contains(Keywords));
+                    ExecSearch(result);
+                }
+                if (Search == "12")
+                {
+                    IEnumerable<KantoPokes> result = p.box1.Where(a => a.Name.Contains(Keywords));
+                    ExecSearch(result);
+                }
+                if (Search == "13")
+                {
+                    IEnumerable<KantoPokes> result = p.box1.Where(a => a.Type1.Contains(Keywords));
+                    ExecSearch(result);
+                }
+                if (Search == "21")
+                {
+                    IEnumerable<JohtoPokes> result = p.box2.Where(a => a.DexNo.Contains(Keywords));
+                    ExecSearch(result);
+                }
+                if (Search == "22")
+                {
+                    IEnumerable<JohtoPokes> result = p.box2.Where(a => a.Name.Contains(Keywords));
+                    ExecSearch(result);
+                }
+                if (Search == "23")
+                {
+                    IEnumerable<JohtoPokes> result = p.box2.Where(a => a.Type1.Contains(Keywords));
+                    ExecSearch(result);
+                }
+            }
         }
 
-        static void GetAll(Program p)
-        {
-            foreach (KantoPokes i in p.box1)
-            {
-                Console.WriteLine("Dex no.: {0}\t\tName: {1}\t\t\tType: {2} {3}",
-                    i.DexNo, i.Name, i.Type1, i.Type2);
-            }
-
-            foreach (JohtoPokes i in p.box2)
-            {
-                Console.WriteLine("Dex no.: {0}\t\tName: {1}\t\t\tType: {2} {3}",
-                    i.DexNo, i.Name, i.Type1, i.Type2);
-            }
-        }
         static string Queries(Program p)
         {
             foreach (KantoPokes i in p.box1)
@@ -114,71 +106,6 @@ namespace InterfacesAndLINQ
             }
             return "1";
         }
-        static string SearchBy(string region)
-        {
-            string input;
-            while (true)
-            {
-                Console.WriteLine("Search by:");
-                Console.WriteLine("[1] DexNo, [2] Name, [3] Type, [4] List all");
-                Console.Write(">>  ");
 
-                input = Console.ReadLine();
-
-                if (input == "1" || input == "2" || input == "3" || input == "4")
-                {
-                    //Console.WriteLine(region + "" + input);
-                    return region + "" + input;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input. Try again.\n\n");
-                }
-
-            }
-        }
-        static string ChooseRegion()
-        {
-            string input;
-            while (true)
-            {
-                Console.WriteLine("Choose a region:");
-                Console.WriteLine("[1] Kanto, [2] Johto");
-                Console.Write(">>  ");
-
-                input = Console.ReadLine();
-
-                if (input == "1" || input == "2")
-                {
-                    return input;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input. Try again.\n\n");
-                }
-
-            }
-        }
-        static string GetSearchKeywords()
-        {
-            Console.Write("Search: ");
-            return Console.ReadLine();
-        }
-        static void ExecSearch(IEnumerable<KantoPokes> result)
-        {
-            foreach (var i in result)
-            {
-                Console.WriteLine("Dex no.: {0}\t\tName: {1}\t\t\tType: {2} {3}",
-                    i.DexNo, i.Name, i.Type1, i.Type2);
-            }
-        }
-        static void ExecSearch(IList<JohtoPokes> result)
-        {
-            foreach (var i in result)
-            {
-                Console.WriteLine("Dex no.: {0}\t\tName: {1}\t\t\tType: {2} {3}",
-                    i.DexNo, i.Name, i.Type1, i.Type2);
-            }
-        }
     }
 }
