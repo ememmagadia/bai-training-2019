2b20d0fa8055dca721d1d68a9fcc3385fc0060b8
Emmanuel P. Magadia <emmanuelmagadia@outlook.com>
2019-05-31 12:11:33 +0800

    demo commit

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -19,6 +19,10 @@ namespace CSharpBasics //namespace declarations,  namespace is a collection of c
 
 
             Console.ReadLine();
+            Console.ReadLine();
+            Console.ReadLine();
+            Console.ReadLine();
+
         }
     }
 }
